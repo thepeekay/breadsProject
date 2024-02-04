1b0f9d5f6ab5256c8c09d82cb22bb7bc593dc488
labbeltran <labbeltran@gmail.com>
2024-02-04 12:45:13 -0500

    donzo

--- views/bakerShow.jsx
+++ views/bakerShow.jsx
@@ -1,5 +1,5 @@
 const React = require('react')
-const Default = require('./layouts/Default')
+const Default = require('./layouts/default')
 
 function Show ({baker}) {
     return (
@@ -8,6 +8,15 @@ function Show ({baker}) {
           <p>{baker.name} has been baking with us since {baker.startDate.getFullYear()}</p>
           <p>About {baker.name}: {baker.bio}</p>
           <h3>Breads {baker.name} has baked</h3>
+          <ul>
+            {
+                baker.breads.map((bread)=>{
+                    return(
+                        <li key={bread.id}>{bread.name}</li>
+                    )
+                })
+            }
+          </ul>
       </Default>
     )
 }
--- views/index.jsx
+++ views/index.jsx
@@ -1,11 +1,12 @@
 const React = require('react')
 const Default = require('./layouts/default')
 
-function Index ({breads, bakers, title})  {
+function Index({breads, bakers, title}) {
     return (
-    <Default title={title}>
+      <Default title={title}>
+        
         <h2>Index Page</h2>
-        <h3>Bakers</h3>
+        <h3>Boulangers</h3>
         <ul>
             {
                 bakers.map((baker)=> {
@@ -17,11 +18,26 @@ function Index ({breads, bakers, title})  {
                 })
             }
         </ul>
-        <h3>Breads</h3>
-        [ ... ]
-    </Default>
+        <h3>Pans</h3>
+        <ul>
+            {
+                breads.map((bread, index) => {
+                    return(
+                        <li key={index}>
+                            <a href={`/breads/${bread.id}`}>
+                            {bread.name}
+                            </a>
+                        </li>
+                    )
+                })
+            }
+        </ul>
+        <div className="newButton">
+            <a href="/breads/new"><button>Add a new bread</button></a>
+        </div>
+
+      </Default>
     )
 }
-
 
 module.exports = Index
