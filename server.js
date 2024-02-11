756db5626e094a0761321306a653797518117917
thepeekay <panagiotis@kefalidis.me>
2024-02-11 13:04:44 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -35,16 +35,9 @@ app.get('*', (req, res) => {
   res.render('404')
 })
   
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log('connected to mongo: ', process.env.MONGO_URI))
 
 // LISTEN
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
-
-mongoose.connect(process.env.MONGO_URI)
-console.log('connect to mongo:' , process.env.MONGO_URI)
-
-mongoose.createConnection(process.env.MONGO_URI).asPromise(
-  console.log('connect to mongo: ', process.env.MONGO_URI))
-
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log('connected to mongo: ', process.env.MONGO_URI))
